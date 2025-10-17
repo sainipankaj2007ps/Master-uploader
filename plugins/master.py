bed8e238f0ae9ee0413b1d4b0e1061120791bccd
sainipankaj2007ps <raindropgbstar@gmail.com>
2025-10-17 16:47:49 +0530

    Update master.py

--- plugins/master.py
+++ plugins/master.py
@@ -55,7 +55,7 @@ async def account_login(bot, m):
         else:
             MR = raw_text3
 
-        token = f"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NDI2MzYxNTkuNTc4LCJkYXRhIjp7Il9pZCI6IjY1OWZjZWU5YmI4YjFkMDAxOGFmYTExZCIsInVzZXJuYW1lIjoiODUzOTkyNjE5MCIsImZpcnN0TmFtZSI6IlNoaXR0dSIsImxhc3ROYW1lIjoiU2luZ2giLCJvcmdhbml6YXRpb24iOnsiX2lkIjoiNWViMzkzZWU5NWZhYjc0NjhhNzlkMTg5Iiwid2Vic2l0ZSI6InBoeXNpY3N3YWxsYWguY29tIiwibmFtZSI6IlBoeXNpY3N3YWxsYWgifSwiZW1haWwiOiJzaGl0dHVrdW1hcjM3QGdtYWlsLmNvbSIsInJvbGVzIjpbIjViMjdiZDk2NTg0MmY5NTBhNzc4YzZlZiJdLCJjb3VudHJ5R3JvdXAiOiJJTiIsInR5cGUiOiJVU0VSIn0sImlhdCI6MTc0MjAzMTM1OX0.HhM0JtZEyI4Laed4oQCmGkLjP-_5SvqqS1w-o5ZUbdU"
+        token = f"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NjEyOTQxNDkuMTQzLCJkYXRhIjp7Il9pZCI6IjY1YjkwM2Y1MTFiMGMwZjA1MTc4YTk5YSIsInVzZXJuYW1lIjoiNzY5MDg0NTYwNCIsImZpcnN0TmFtZSI6Ikxva2VzaCIsImxhc3ROYW1lIjoiU2FpbmkiLCJvcmdhbml6YXRpb24iOnsiX2lkIjoiNWViMzkzZWU5NWZhYjc0NjhhNzlkMTg5Iiwid2Vic2l0ZSI6InBoeXNpY3N3YWxsYWguY29tIiwibmFtZSI6IlBoeXNpY3N3YWxsYWgifSwicm9sZXMiOlsiNWIyN2JkOTY1ODQyZjk1MGE3NzhjNmVmIl0sImNvdW50cnlHcm91cCI6IklOIiwidHlwZSI6IlVTRVIifSwiaWF0IjoxNzYwNjg5MzQ5fQ.RICx37PhQ5zIhkgxqGLNnrRI_YHAY2bjrIve8WFOojs"
         await editable.edit("Now send the __Thumb URL__\n\nor Send `no`")
         input6 = await bot.listen(chat_id=m.chat.id, filters=filters.text & filters.user(m.from_user.id))
         thumb = input6.text
@@ -79,3 +79,4 @@ async def account_login(bot, m):
     except Exception as e:
         await m.reply_text(f"**⚠️Downloading Failed⚠️**\n\n**Fail Reason »** {e}\n\n**╰────⌈✨❤️ 『{Credit}』 ❤️✨**⌋────╯")
         return
+
